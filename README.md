a45174bd3cdec0a14be7d0c275bde4a188c257e5
Jonnathan Baculima <jonnabcl56@gmail.com>
2022-07-04 21:28:13 -0500

    Initial commit + install dependencies + server with express

---
+++ README.md
@@ -0,0 +1,9 @@
+# Test Ecommerce Backend withn Node
+
+## **Dependencies**
+  - express
+  - mongoose
+  - dotenv
+  - nodemon
+
+Run npm install or yarn to install dependencies.
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('Hello world!')
+})
+
+app.listen(3001, () => {
+  console.log('Server is running in port 3001');
+});
