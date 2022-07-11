435704862a23b1e164cc8afec54749096d12585b
Jonnathan Baculima <jonnabcl56@gmail.com>
2022-07-10 19:18:12 -0500

    Stripe integration + cors error

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 require('./mongo')
 
 const express = require('express')
+const cors = require('cors')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const productRouter = require('./controllers/products')
 const cartRouter = require('./controllers/cart')
 const orderRouter = require('./controllers/orders')
+const stripeRouter = require('./controllers/stripe')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/login', loginRouter)
@@ -16,6 +19,7 @@ app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/orders', orderRouter)
+app.use('/api/checkout', stripeRouter)
 
 const PORT = process.env.PORT || 3005
 
